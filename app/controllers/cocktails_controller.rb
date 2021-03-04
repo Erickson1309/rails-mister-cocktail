0e6c3ef7b1b4a430f47360fe0eb66dd1d50c963f
Erickson P <joelerickson@hotmail.co.uk>
2021-03-04 23:01:55 +0000

    added CSS, cleaned up views, routes and controllers

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,26 +1,26 @@
 class CocktailsController < ApplicationController
-
   def index
     @cocktails = Cocktail.all
   end
 
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    # @review = Review.new
+  end
+
   def new
-    @cocktails = Cocktail.new
+    @cocktail = Cocktail.new
   end
 
   def create
-    @cocktails = Cocktail.new(cocktail_params)
-    if @cocktails.save
-      redirect_to cocktails_path(@cocktails)
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
     else
       render 'new'
     end
   end
-
-  def show
-    @cocktail = Cocktail.find(params[:id])
-  end
-
 
   private
 
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,4 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :cocktail, uniqueness: { scope: :ingredient }
-  validates :description, presence: true
 end
